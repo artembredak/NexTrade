68ec8843973205265f902674a74826519543b1ee
artembredak <artembredakk@gmail.com>
2026-04-10 22:47:22 +0200

    initial commit

---
+++ src/main/java/com/artembredak/nextrade/NexTradeApplication.java
@@ -0,0 +1,13 @@
+package com.artembredak.nextrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artembredak/nextrade/NexTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artembredak.nextrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
